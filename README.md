6dc33e2bce28d2a2d015886812c97d2729d9ea17
Senor101 <adikshyanta456@gmail.com>
2023-10-12 14:16:55 +0545

    README init

---
+++ README.md
@@ -0,0 +1,9 @@
+# NODE OAUTH
+
+### Tech Stacks used:
+
+- Passport
+- JWT
+- Passport-jwt
+- Passport-google-oauth20
+- passport-facebook
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
+const customErrorhandler = require("./middleware/errorhandler.middleware");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
